be3f895e5fa5d5985d3fc8be5d1676ea56d98b66
Kaine van Geffen <78799007+KVGFFN@users.noreply.github.com>
2022-03-31 13:26:17 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    // Landscape Orientation
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: const Color.fromARGB(22, 22, 22, 1)),
+      home: const MyHomePage(title: 'Gradeaid'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(0, 59, 49, 49),
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: [
+            const SizedBox(height: 100),
+            LoginText(),
+            const SizedBox(height: 20),
+            MailInput(),
+            const SizedBox(height: 20),
+            PasswordInput(),
+            const SizedBox(height: 20),
+            LoginButton()
+          ],
+        ));
+  }
+}
+
+class LoginText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: const Text("Student Login",
+          style: TextStyle(
+              color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold)),
+    );
+  }
+}
+
+class MailInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+            width: 400,
+            child: const TextField(
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "AP E-Mail",
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+                hintStyle: TextStyle(color: Colors.grey),
+                fillColor: Color.fromARGB(80, 61, 61, 61),
+                filled: true,
+              ),
+            )));
+  }
+}
+
+class PasswordInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 400,
+        child: const TextField(
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              hintText: "Password",
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              hintStyle: TextStyle(color: Colors.grey),
+              fillColor: Color.fromARGB(80, 61, 61, 61),
+              filled: true,
+            )));
+  }
+}
+
+class LoginButton extends StatefulWidget {
+  @override
+  State<LoginButton> createState() => _State();
+}
+
+class _State extends State<LoginButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 400,
+        child: TextButton(
+          onPressed: () {},
+          child: Text("Login"),
+          style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.all(Colors.white),
+              backgroundColor: MaterialStateProperty.all(Colors.red)),
+        ));
+  }
+}
