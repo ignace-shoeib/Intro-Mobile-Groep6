8638c0307a146be6bae3eca5e0e5359a91ee4173
Kaine van Geffen <78799007+KVGFFN@users.noreply.github.com>
2022-04-27 22:05:26 +0200

    x

--- lib/main.dart
+++ lib/main.dart
@@ -182,22 +182,3 @@ class _LoginAsAdminState extends State<LoginAsAdminButton> {
             )));
   }
 }
-<<<<<<< HEAD
-
-var _sampleStudents = [
-  "s000000;bart",
-  "s000111;albert",
-  "s000222;frankcallaerts"
-];
-
-List<DropdownMenuItem<String>> get dropdownItems {
-  List<DropdownMenuItem<String>> menuItems = [
-    DropdownMenuItem(child: Text("USA"), value: "USA"),
-    DropdownMenuItem(child: Text("Canada"), value: "Canada"),
-    DropdownMenuItem(child: Text("Brazil"), value: "Brazil"),
-    DropdownMenuItem(child: Text("England"), value: "England"),
-  ];
-  return menuItems;
-}
-=======
->>>>>>> parent of 61291ed (add list of samplestudents)
