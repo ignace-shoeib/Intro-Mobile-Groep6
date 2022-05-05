723b31c3315fbdf2ca3d8897ff09eed053509b81
Kaine van Geffen <78799007+KVGFFN@users.noreply.github.com>
2022-05-05 23:20:38 +0200

    edited admin_codecorrection - multines size

--- lib/admin_codecorrection.dart
+++ lib/admin_codecorrection.dart
@@ -20,69 +20,83 @@ class CodeCorrectionPageState extends State<CodeCorrectionPage> {
         //
         // body
         //
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: Column(
           children: [
-            //
-            // CODE VRAAG
-            //
-            Expanded(
-              child: Column(
-                children: [
-                  SizedBox(height: 20),
-                  const Text("Vraag toevoegen",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold)),
-                  SizedBox(height: 20),
-                  TextFormField(
-                      controller: questionController,
-                      keyboardType: TextInputType.multiline,
-                      maxLines: 25,
-                      style: const TextStyle(color: Colors.white),
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "Vraag",
-                        labelText: "Vraag",
-                        floatingLabelBehavior: FloatingLabelBehavior.always,
-                        hintStyle: TextStyle(color: Colors.grey),
-                        fillColor: Color.fromARGB(80, 61, 61, 61),
-                        filled: true,
-                      )),
-                ],
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //
+                // CODE VRAAG
+                //
+                Expanded(
+                  child: Column(
+                    children: [
+                      SizedBox(height: 20),
+                      const Text("Vraag toevoegen",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold)),
+                      SizedBox(height: 20),
+                      TextFormField(
+                          controller: questionController,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: 10,
+                          style: const TextStyle(color: Colors.white),
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "Vraag",
+                            labelText: "Vraag",
+                            floatingLabelBehavior: FloatingLabelBehavior.always,
+                            hintStyle: TextStyle(color: Colors.grey),
+                            fillColor: Color.fromARGB(80, 61, 61, 61),
+                            filled: true,
+                          )),
+                    ],
+                  ),
+                ),
+                //
+                // CORRECTE CODE
+                //
+                Expanded(
+                  child: Column(
+                    children: [
+                      SizedBox(height: 20),
+                      const Text("Correcte code",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold)),
+                      SizedBox(height: 20),
+                      TextFormField(
+                          controller: correctCodeController,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: 10,
+                          style: const TextStyle(color: Colors.white),
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "Correcte code",
+                            labelText: "Correcte code",
+                            floatingLabelBehavior: FloatingLabelBehavior.always,
+                            hintStyle: TextStyle(color: Colors.grey),
+                            fillColor: Color.fromARGB(80, 61, 61, 61),
+                            filled: true,
+                          )),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            //
-            // CORRECTE CODE
-            //
-            Expanded(
-              child: Column(
-                children: [
-                  SizedBox(height: 20),
-                  const Text("Correcte code",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold)),
-                  SizedBox(height: 20),
-                  TextFormField(
-                      controller: correctCodeController,
-                      keyboardType: TextInputType.multiline,
-                      maxLines: 25,
-                      style: const TextStyle(color: Colors.white),
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "Correcte code",
-                        labelText: "Correcte code",
-                        floatingLabelBehavior: FloatingLabelBehavior.always,
-                        hintStyle: TextStyle(color: Colors.grey),
-                        fillColor: Color.fromARGB(80, 61, 61, 61),
-                        filled: true,
-                      )),
-                ],
-              ),
-            ),
+            SizedBox(height: 40),
+            SizedBox(
+                width: 400,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text("Vraag opslaan"),
+                  style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.all(Colors.white),
+                      backgroundColor: MaterialStateProperty.all(Colors.red)),
+                ))
           ],
         ));
   }
