aa90b776b012462774428584b6100ce81fc44024
StudentAD <s124568@ap.be>
2022-05-05 20:01:30 +0200

    edt student vragen page

--- lib/StudentVragenPage.dart
+++ lib/StudentVragenPage.dart
@@ -13,88 +13,72 @@ class StudentVragenPage extends StatelessWidget {
           title: const Text("Gradeaid"),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            AdminLoginTitle(),
-            Expanded(
-                child: Align(
-              alignment: FractionalOffset.bottomCenter,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                    foregroundColor: MaterialStateProperty.all(Colors.white),
-                    backgroundColor: MaterialStateProperty.all(Colors.red)),
-                child: EndExamPopUp(context),
-
-                /*const Text('Beëindig examen'),
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return Dialog(
-                            child: Container(
-                                height: 300.0,
-                                width: 300.0,
-                                child: Column(
-                                  children: <Widget>[
-                                    const Text(
-                                        'Wilt u zeker het examen beëindigen?'),
-                                    TextButton(
-                                      child: const Text('Ja'),
-                                      onPressed: () {},
-                                    ),
-                                    TextButton(
-                                      child: const Text('Nee'),
-                                      onPressed: () {},
-                                    )
-                                  ],
-                                )));
-                      });*/
-              ),
-            ))
-          ],
-        ));
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const AdminLoginTitle(),
+              Expanded(
+                  child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.all(Colors.white),
+                      backgroundColor: MaterialStateProperty.all(Colors.red)),
+                  child: const Text('Beëindig examen'),
+                  onPressed: () {
+                    showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                              backgroundColor: Color.fromARGB(255, 22, 22, 22),
+                              title: const Text(
+                                  'Wilt u zeker het examen beëindigen?',
+                                  style: TextStyle(
+                                      color:
+                                          Color.fromARGB(255, 255, 255, 255))),
+                              /*content: const Text(
+                                  'Gebruikersnaam of wachtwoord is onjuist',
+                                  style: TextStyle(
+                                      color:
+                                          Color.fromARGB(255, 255, 255, 255))),*/
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () =>
+                                      Navigator.pop(context, 'Nee'),
+                                  child: const Text(
+                                    'Nee',
+                                    style: TextStyle(color: Colors.red),
+                                  ),
+                                ),
+                                ElevatedButton(
+                                  style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all(
+                                              Colors.red)),
+                                  onPressed: () => Navigator.pop(context, 'Ja'),
+                                  child: const Text(
+                                    'Ja',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ],
+                            ));
+                  },
+                ),
+              ))
+            ]));
   }
 }
 
-EndExamPopUp(BuildContext context) => Container();
-
 class AdminLoginTitle extends StatelessWidget {
+  const AdminLoginTitle({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Align(
+    return const Align(
       alignment: Alignment.bottomCenter,
-      child: const Text("Vragen",
+      child: Text("Vragen",
           style: TextStyle(
               color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold)),
     );
   }
 }
-
-/*
-class StudentEndExamButton extends StatefulWidget {
-  @override
-  State<StudentEndExamButton> createState() => StudentEndExamButtonState();
-}
-
-class StudentEndExamButtonState extends State<StudentEndExamButton> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        width: 400,
-        child: TextButton(
-          onPressed: () {
-            openDialog();
-          },
-          child: Text("Examen Beëindigen"),
-          style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.all(Colors.white),
-              backgroundColor: MaterialStateProperty.all(Colors.red)),
-        ));
-  }
-}
-
-Future openDialog() => showDialog(
-      context: context,
-      builder: (context) => AlertDialog(),
-    );*/
