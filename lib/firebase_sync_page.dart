c0d10011d0e5bca8560e2a7df000a33b3933fab5
Ignace Shoeib <ignace.shoeib@student.ap.be>
2022-05-11 15:42:55 +0200

    upload/download students

--- lib/firebase_sync_page.dart
+++ lib/firebase_sync_page.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+import 'package:exam_app/message_box.dart';
+import 'package:exam_app/student/load_students.dart';
+import 'package:exam_app/student/student.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class FirebaseSync extends StatelessWidget {
@@ -35,7 +40,20 @@ class StudentUploadState extends State<StudentUploadButton> {
     return SizedBox(
         width: 400,
         child: TextButton(
-          onPressed: () {},
+          onPressed: () async {
+            for (var i = 0; i < LoadStudents.students.length; i++) {
+              DatabaseReference ref =
+                  FirebaseDatabase.instance.ref("students/$i");
+              await ref.set({
+                "studentNr": LoadStudents.students[i].studentNr,
+                "studentName": LoadStudents.students[i].studentName,
+              });
+            }
+            MessageBox.showMessageBox(
+                "Succes",
+                "De studenten zijn succesvol geüpload naar de database.",
+                context);
+          },
           child: const Text("Upload studenten"),
           style: ButtonStyle(
               foregroundColor: MaterialStateProperty.all(Colors.white),
@@ -57,7 +75,23 @@ class StudentDownloadState extends State<StudentDownloadButton> {
     return SizedBox(
         width: 400,
         child: TextButton(
-          onPressed: () {},
+          onPressed: () async {
+            final ref = FirebaseDatabase.instance.ref();
+            final snapshot = await ref.child("students").get();
+            var encoded = json.encode(snapshot.value);
+            List<dynamic> encodedStudents = json.decode(encoded);
+            List<Student> students = [];
+            for (var i = 0; i < encodedStudents.length; i++) {
+              students.add(Student(
+                  studentNr: encodedStudents[i]["studentNr"],
+                  studentName: encodedStudents[i]["studentName"]));
+            }
+            LoadStudents.students = students;
+            MessageBox.showMessageBox(
+                "Succes",
+                "De studenten zijn succesvol gedownload van de database.",
+                context);
+          },
           child: const Text("Download studenten"),
           style: ButtonStyle(
               foregroundColor: MaterialStateProperty.all(Colors.white),
