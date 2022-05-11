bf75f1f7c7de51c40a77b5ce9543b3f08db7a41d
Ignace Shoeib <ignace.shoeib@student.ap.be>
2022-05-11 13:58:52 +0200

    refresh home page

--- lib/admin/admin_login_page.dart
+++ lib/admin/admin_login_page.dart
@@ -1,3 +1,4 @@
+import 'package:exam_app/my_app.dart';
 import 'package:flutter/material.dart';
 import 'admin_form.dart';
 
@@ -6,20 +7,32 @@ class AdminLoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(0, 0, 0, 0),
-          title: const Text("Gradeaid"),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            AdminLoginTitle(),
-            SizedBox(height: 20),
-            AdminLogin(),
-            LoginAsStudentButton(),
-          ],
-        ));
+    return WillPopScope(
+        onWillPop: () async {
+          Navigator.pop(context);
+          Navigator.pop(context);
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const MyApp(),
+            ),
+          );
+          return false;
+        },
+        child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: const Color.fromARGB(0, 0, 0, 0),
+              title: const Text("Gradeaid"),
+            ),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                AdminLoginTitle(),
+                SizedBox(height: 20),
+                AdminLogin(),
+                LoginAsStudentButton(),
+              ],
+            )));
   }
 }
