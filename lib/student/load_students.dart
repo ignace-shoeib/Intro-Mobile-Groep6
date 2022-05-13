eb899f538ddb21cf16ca22f073230c017c451d0d
Ignace Shoeib <ignace.shoeib@student.ap.be>
2022-05-13 14:15:53 +0200

    try

--- lib/student/load_students.dart
+++ lib/student/load_students.dart
@@ -1,4 +1,7 @@
 import 'dart:io';
+import 'package:exam_app/message_box.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:csv/csv.dart';
 import 'package:path_provider/path_provider.dart';
@@ -9,42 +12,57 @@ class LoadStudents {
   static Future<List<Student>> loadStudentsFirstTime() async {
     final file = await _localFile;
     var exists = await file.exists();
-    if (!exists) {
-      final _rawData = await rootBundle.loadString("assets/data.csv");
-      List<List<dynamic>> _listData =
-          const CsvToListConverter().convert(_rawData);
+    try {
+      if (!exists) {
+        final _rawData = await rootBundle.loadString("assets/data.csv");
+        List<List<dynamic>> _listData =
+            const CsvToListConverter().convert(_rawData);
 
-      final csv = CsvCodec();
-      await file.writeAsString(csv.encoder.convert(_listData));
-      List<List<String>> splittedCSV = [];
-      final splittedData = _rawData.split("\n");
-      for (var item in splittedData) {
-        splittedCSV.add(item.toString().split(";"));
-      }
-      List<Student> students = [];
-      for (var item in splittedCSV) {
-        if (item.length >= 2) {
+        final csv = CsvCodec();
+        await file.writeAsString(csv.encoder.convert(_listData));
+        List<List<String>> splittedCSV = [];
+        final splittedData = _rawData.split("\n");
+        for (var item in splittedData) {
+          splittedCSV.add(item.toString().split(";"));
+        }
+        List<Student> students = [];
+        for (var item in splittedCSV) {
+          if (item.length >= 2) {
+            students.add(Student(
+              studentNr: item[0].toString().replaceAll('[', ''),
+              studentName: item[1].toString().replaceAll(']', ''),
+            ));
+          }
+        }
+        return students;
+      } else {
+        final _rawData = await file.readAsString();
+        final splittedData = _rawData.split("\n");
+        List<List<String>> splittedCSV = [];
+        for (var item in splittedData) {
+          splittedCSV.add(item.toString().split(";"));
+        }
+        List<Student> students = [];
+        for (var item in splittedCSV) {
           students.add(Student(
             studentNr: item[0].toString().replaceAll('[', ''),
             studentName: item[1].toString().replaceAll(']', ''),
           ));
         }
+        return students;
       }
-      return students;
-    } else {
-      final _rawData = await file.readAsString();
-      final splittedData = _rawData.split("\n");
-      List<List<String>> splittedCSV = [];
-      for (var item in splittedData) {
-        splittedCSV.add(item.toString().split(";"));
+    } catch (e) {
+      Widget build(BuildContext context) {
+        return SizedBox(
+            width: 100,
+            child: TextButton(
+                onPressed: () {
+                  MessageBox.showMessageBox("error", e.toString(), context);
+                },
+                child: const Text("Debug")));
       }
+
       List<Student> students = [];
-      for (var item in splittedCSV) {
-        students.add(Student(
-          studentNr: item[0].toString().replaceAll('[', ''),
-          studentName: item[1].toString().replaceAll(']', ''),
-        ));
-      }
       return students;
     }
   }
