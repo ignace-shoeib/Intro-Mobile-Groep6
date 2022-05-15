e3877dba730e3fb9929839e849a53e1a201b8d92
Ignace Shoeib <ignace.shoeib@student.ap.be>
2022-05-15 17:58:00 +0200

    location+address

--- lib/student/student_start_exam_page.dart
+++ lib/student/student_start_exam_page.dart
@@ -33,8 +33,8 @@ class StudentStartExamButtonState extends State<StudentStartExamButton> {
         width: 400,
         child: TextButton(
           onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => const StudentQuestionPage()));
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => StudentQuestionPage()));
           },
           style: ButtonStyle(
               foregroundColor: MaterialStateProperty.all(Colors.white),
