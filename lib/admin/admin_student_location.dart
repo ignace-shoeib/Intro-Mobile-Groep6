79ac7c85bcae8ea329de0646eb0c951ff8733c88
Ignace Shoeib <ignace.shoeib@student.ap.be>
2022-05-15 20:39:39 +0200

    show location of student

--- lib/admin/admin_student_location.dart
+++ lib/admin/admin_student_location.dart
@@ -2,21 +2,40 @@ import 'dart:convert';
 import 'package:exam_app/student/load_students.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 
 class StudentLocationPage extends StatelessWidget {
   const StudentLocationPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    MapController controller = MapController();
     return Scaffold(
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(0, 0, 0, 0),
           title: const Text("Gradeaid"),
         ),
-        body: Column(children: const [
+        body: Column(children: [
           PageTitle(),
           SizedBox(height: 10),
-          LocationText()
+          LocationText(),
+          Flexible(
+              child: OSMFlutter(
+            controller: controller,
+            trackMyPosition: false,
+            initZoom: 16,
+            minZoomLevel: 2,
+            maxZoomLevel: 19,
+            stepZoom: 1.0,
+            onMapIsReady: (ready) async {
+              String data = await GetData.getData();
+              var jsonData = jsonDecode(data);
+              double lat = jsonData["location"]["latitude"];
+              double lon = jsonData["location"]["longitude"];
+              GeoPoint geopoint = GeoPoint(latitude: lat, longitude: lon);
+              await controller.changeLocation(geopoint);
+            },
+          ))
         ]));
   }
 }
