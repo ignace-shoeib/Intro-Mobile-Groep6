ea146ea8e134641c967ec949d80bf85968292ea6
Ignace Shoeib <ignace.shoeib@student.ap.be>
2022-05-16 15:04:03 +0200

    grade students

--- lib/admin/admin_edit_student_score.dart
+++ lib/admin/admin_edit_student_score.dart
@@ -1,5 +1,7 @@
 import 'package:exam_app/admin/admin_studentgradepage.dart';
+import 'package:exam_app/message_box.dart';
 import 'package:exam_app/student/load_students.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class AdminEditStudentScore extends StatelessWidget {
@@ -15,7 +17,7 @@ class AdminEditStudentScore extends StatelessWidget {
         body: Column(
             //mainAxisAlignment: MainAxisAlignment.center,
             //crossAxisAlignment: CrossAxisAlignment.center,
-            children: const [
+            children: [
               EditStudentScoreTitle(),
               Expanded(child: StudentGradeScoreText()),
               Expanded(flex: 0, child: EditStudentScoreInput()),
@@ -47,15 +49,15 @@ class EditStudentScoreTitle extends StatelessWidget {
 
 class EditStudentScoreInput extends StatelessWidget {
   const EditStudentScoreInput({Key? key}) : super(key: key);
-
+  static String score = '';
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
         child: SizedBox(
             width: 200,
             child: TextField(
               // Als je onchanged erbij doet gaat problemen weg
-              //onChanged: (value) => StudentAnswer.answer = value,
+              onChanged: (value) => score = value,
               style: TextStyle(color: Colors.white),
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
@@ -87,7 +89,38 @@ class SaveChangedScoreButtonState extends State<SaveChangedScoreButton> {
               foregroundColor: MaterialStateProperty.all(Colors.white),
               backgroundColor: MaterialStateProperty.all(Colors.red)),
           child: const Text('Bewerking opslaan'),
-          onPressed: () {}),
+          onPressed: () async {
+            DatabaseReference ref = FirebaseDatabase.instance
+                .ref("answers/${LoadStudents.currentStudent.trim()}/metadata");
+            await ref.set({
+              "score": EditStudentScoreInput.score,
+            });
+            showDialog<String>(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                      backgroundColor: const Color.fromARGB(255, 22, 22, 22),
+                      title: Text("succes",
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255))),
+                      content: Text(
+                          "U heeft de score van ${LoadStudents.currentStudent} veranderd naar ${EditStudentScoreInput.score}",
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255))),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context, 'OK');
+                            Navigator.pop(context);
+                            Navigator.pop(context);
+                          },
+                          child: const Text(
+                            'OK',
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ),
+                      ],
+                    ));
+          }),
     );
   }
 }
